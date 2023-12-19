f8e7bc58adb163d4f40a2d531d4abaf24db23f58
dimske <dempsey.de.clerck@hotmail.com>
2023-12-19 13:44:33 +0100

    Fix handler

--- src/App/Exceptions/Handler.php
+++ src/App/Exceptions/Handler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Psr\Log\LoggerInterface;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,11 +36,31 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Logger instance.
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        parent::__construct();
+        $this->logger = $logger;
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
+            // Log the exception
+            $this->logger->error($e->getMessage(), ['exception' => $e]);
         });
     }
 }
