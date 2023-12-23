e28a6681ff9be825a11ebf81023385a6bfa36c89
Dimske <dempsey.de.clerck@hotmail.com>
2023-12-23 05:16:41 +0100

    Update AiTextServices.php

--- Enums/AiTextServices.php
+++ Enums/AiTextServices.php
@@ -10,71 +10,36 @@ namespace App\Enums;
  */
 class AiTextServices
 {
-
     // /v1/chat/completions ENDPOINT
 
-    /** @var string Represents the GPT-4 model. */
-    const GPT_4 = "gpt-4";
-
-    /** @var string Represents the GPT-4 0613 model. */
-    const GPT_4_0613 = "gpt-4-0613";
-
-    /** @var string Represents the GPT-4 32K model. */
-    const GPT_4_32K = "gpt-4-32k";
-
-    /** @var string Represents the GPT-4 32K 0613 model. */
-    const GPT_4_32K_0613 = "gpt-4-32k-0613";
-
-    /** @var string Represents the GPT-3.5 Turbo model. */
-    const GPT_35_TURBO = "gpt-3.5-turbo";
-
-    /** @var string Represents the GPT-3.5 Turbo 0613 model. */
-    const GPT_35_TURBO_0613 = "gpt-3.5-turbo-0613";
-
-    /** @var string Represents the GPT-3.5 Turbo 16K model. */
-    const GPT_35_TURBO_16K = "gpt-3.5-turbo-16k";
-
-    /** @var string Represents the GPT-3.5 Turbo 16K 0613 model. */
-    const GPT_35_TURBO_16K_0613 = "gpt-3.5-turbo-16k-0613";
+    public const GPT_4 = 'gpt-4';
+    public const GPT_4_0613 = 'gpt-4-0613';
+    public const GPT_4_32K = 'gpt-4-32k';
+    public const GPT_4_32K_0613 = 'gpt-4-32k-0613';
+    public const GPT_35_TURBO = 'gpt-3.5-turbo';
+    public const GPT_35_TURBO_0613 = 'gpt-3.5-turbo-0613';
+    public const GPT_35_TURBO_16K = 'gpt-3.5-turbo-16k';
+    public const GPT_35_TURBO_16K_0613 = 'gpt-3.5-turbo-16k-0613';
 
     // /v1/completions ENDPOINT
 
-    /** @var string Represents the Text Davinci 003 model. */
-    const TEXT_DAVINCI_003 = "text-davinci-003";
-
-    /** @var string Represents the Text Davinci 002 model. */
-    const TEXT_DAVINCI_002 = "text-davinci-002";
-
-    /** @var string Represents the Text Davinci 001 model. */
-    const TEXT_DAVINCI_001 = "text-davinci-001";
-
-    /** @var string Represents the Text Curie 001 model. */
-    const TEXT_CURIE_001 = "text-curie-001";
-
-    /** @var string Represents the Text Babbage 001 model. */
-    const TEXT_BABBAGE_001 = "text-babbage-001";
-
-    /** @var string Represents the Text Ada 001 model. */
-    const TEXT_ADA_001 = "text-ada-001";
-
-    /** @var string Represents the Davinci model. */
-    const DAVINCI = "davinci";
-
-    /** @var string Represents the Curie model. */
-    const CURIE = "curie";
-
-    /** @var string Represents the Babbage model. */
-    const BABBAGE = "babbage";
-
-    /** @var string Represents the Ada model. */
-    const ADA = "ada";
+    public const TEXT_DAVINCI_003 = 'text-davinci-003';
+    public const TEXT_DAVINCI_002 = 'text-davinci-002';
+    public const TEXT_DAVINCI_001 = 'text-davinci-001';
+    public const TEXT_CURIE_001 = 'text-curie-001';
+    public const TEXT_BABBAGE_001 = 'text-babbage-001';
+    public const TEXT_ADA_001 = 'text-ada-001';
+    public const DAVINCI = 'davinci';
+    public const CURIE = 'curie';
+    public const BABBAGE = 'babbage';
+    public const ADA = 'ada';
 
     /**
      * An array of AI model types.
      *
      * @var array
      */
-    const types = [
+    public const TYPES = [
         self::ADA,
         self::BABBAGE,
         self::CURIE,
@@ -90,7 +55,7 @@ class AiTextServices
      *
      * @var array
      */
-    const chatCompletionsEndpoint = [
+    public const CHAT_COMPLETIONS_ENDPOINT = [
         self::GPT_4,
         self::GPT_4_0613,
         self::GPT_4_32K,
@@ -106,7 +71,7 @@ class AiTextServices
      *
      * @var array
      */
-    const completionsEndpoint = [
+    public const COMPLETIONS_ENDPOINT = [
         self::TEXT_DAVINCI_003,
         self::TEXT_DAVINCI_002,
         self::TEXT_DAVINCI_001,
