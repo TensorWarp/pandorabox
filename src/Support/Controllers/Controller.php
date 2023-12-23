3750e0519ba1bfa99568b1b2465a466f47d89917
Dimske <dempsey.de.clerck@hotmail.com>
2023-12-23 05:53:55 +0100

    Update Controller.php

--- src/Support/Controllers/Controller.php
+++ src/Support/Controllers/Controller.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Support\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests;
-    use ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
